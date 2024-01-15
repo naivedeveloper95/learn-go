a3c5589f535dc467ce0690dbe0cd5b12a167e739
Satyam Suman <findsatyam95@gmail.com>
2024-01-16 00:34:25 +0530

    Overflow in GO

---
+++ overflow/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var x uint8 = 255
+	x++
+	fmt.Println(x)
+
+	// a := int8(255 + 1)
+
+	var b int8 = 127
+	fmt.Printf("%d\n", b+1)
+
+	b = -128
+	b--
+
+	fmt.Printf("%d\n", b)
+
+	f := float32(math.MaxFloat32)
+	fmt.Println(f)
+
+	f = f * 1.2
+	fmt.Println(f)
+
+	// const i int8 = 300
+
+}
