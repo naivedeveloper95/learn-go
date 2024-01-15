3cb255ac11073b24fe5df072ec391655fab7b6ec
Satyam Suman <findsatyam95@gmail.com>
2024-01-15 23:50:00 +0530

    Complex types in GO

--- types/main.go
+++ types/main.go
@@ -30,4 +30,39 @@ func main() {
 	var s string = "Hello Go!"
 	fmt.Printf("%T \n", s)
 
+	// ARRAY TYPE
+	var numbers = [4]int{4, 5, -9, 100}
+	fmt.Printf("%T\n", numbers)
+
+	// SLICE TYPE
+	var cities = []string{"London", "Tokyo", "New York"}
+	fmt.Printf("%T\n", cities)
+
+	// MAP TYPE
+	balances := map[string]float64{
+		"USD": 2332.2,
+		"EUR": 511.11,
+	}
+	fmt.Printf("%T\n", balances)
+
+	// STRUCT TYPE
+	type Person struct {
+		name string
+		age  int
+	}
+
+	var you Person
+	fmt.Printf("%T \n", you)
+
+	// POINTER TYPE
+	var x int = 2
+	ptr := &x
+	fmt.Printf("ptr is of type %T with the value of %v\n", ptr, ptr)
+
+	// FUNCTION TYPE
+	fmt.Printf("%T\n", f)
+}
+
+func f() {
+
 }
