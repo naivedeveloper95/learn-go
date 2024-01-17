fe4c2cd528127024650e747ea1ae498bf41dfbf8
Satyam Suman <findsatyam95@gmail.com>
2024-01-18 00:23:11 +0530

    Label statements in GO

---
+++ label_statements/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	outer := 19
+	_ = outer
+	people := [5]string{"Helen", "Mark", "Brent", "Antonio", "Micheal"}
+	friends := [2]string{"Mark", "Marry"}
+
+outer:
+	for index, name := range people {
+		for _, friend := range friends {
+			if name == friend {
+				fmt.Printf("Found a friend %q at index %d\n", friend, index)
+				break outer
+			}
+		}
+	}
+
+	fmt.Println("Next instruction after the break")
+}
