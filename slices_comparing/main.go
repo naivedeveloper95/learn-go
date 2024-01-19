84d95ff3079a7ff06ede974f9b58e3084608975e
Satyam Suman <findsatyam95@gmail.com>
2024-01-20 01:03:34 +0530

    Slice comparision in GO

---
+++ slices_comparing/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n []int
+	fmt.Println(n == nil)
+
+	m := []int{}
+	fmt.Println(m == nil)
+
+	a, b := []int{1, 2, 3}, []int{1, 5, 3}
+	// fmt.Println(a == b)
+	var eq bool = true
+	for i, v := range a {
+		if v != b[i] {
+			eq = false
+			break
+		}
+	}
+
+	if len(a) != len(b) {
+		eq = false
+	}
+
+	if eq {
+		fmt.Println("a and b slices are equal")
+	} else {
+		fmt.Println("a and b slices are not equal")
+	}
+
+}
