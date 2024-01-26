7af2a207ff9c03c32feafe37c4b32e4143a97cba
Satyam Suman <findsatyam95@gmail.com>
2024-01-26 20:28:33 +0530

    Anonymous Function in GO

---
+++ anonymous_functions/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func increment(x int) func() int {
+	return func() int {
+		x++
+		return x
+	}
+}
+
+func main() {
+	func(msg string) {
+		fmt.Println(msg)
+	}("I'am an anonymous function")
+
+	a := increment(10)
+	fmt.Printf("%T\n", a)
+	a()
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+}
